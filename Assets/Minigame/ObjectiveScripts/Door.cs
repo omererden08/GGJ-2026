2208b2533c4a43ede8b5f4a4f9a2a96351e2bd7c
Ömer Erden <omererden08@outlook.com>
2026-02-01 15:48:27 +0300

    minigamecomplete

--- Assets/Minigame/ObjectiveScripts/Door.cs
+++ Assets/Minigame/ObjectiveScripts/Door.cs
@@ -2,19 +2,46 @@
 
 public class Door : MonoBehaviour
 {
+    [SerializeField] private string sceneName;
+    public enum DoorState
+    {
+        Locked,
+        Open
+    }
+
+    [Header("Door Settings")]
+    public DoorState doorState = DoorState.Locked;
+
+    private void Start()
+    {
+        Key.isCollected = false;
+    }
+
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (!collision.gameObject.CompareTag("Player")) return;
+
+        switch (doorState)
         {
-            if (Key.isCollected)
-            {
-                Debug.Log("🚪 Kapı açıldı!");
-                // İstersen burada animasyon, sahne geçişi, vs. yap
-            }
-            else
-            {
-                Debug.Log("🔒 Kapı kilitli. Anahtar lazım.");
-            }
+            case DoorState.Open:
+                Debug.Log("🚪 Kapı zaten açık. Geçebilirsin.");
+                SceneLoader.Instance.LoadScene(sceneName, GameState.Playing);
+                break;
+
+            case DoorState.Locked:
+                if (Key.isCollected)
+                {
+                    SceneLoader.Instance.LoadScene(sceneName, GameState.Playing);
+                    // Burada istersen kapıyı açabilirsin
+                    doorState = DoorState.Open;
+                    // İsteğe bağlı animasyon, ses, sahne geçişi vs.
+                }
+                else
+                {
+                    Debug.Log("🔒 Kapı kilitli. Anahtar lazım.");
+                }
+                break;
         }
     }
 }
